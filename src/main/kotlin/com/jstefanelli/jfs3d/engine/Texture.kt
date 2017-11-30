09f64a02c060e430c55eaf783695cfaabc726103
jstefanelli <jpsdzn@gmail.com>
2017-11-30 15:13:17 +0100

    Add memory safety to Texture

--- src/main/kotlin/com/jstefanelli/jfs3d/engine/Texture.kt
+++ src/main/kotlin/com/jstefanelli/jfs3d/engine/Texture.kt
@@ -1,38 +1,33 @@
 package com.jstefanelli.jfs3d.engine
 
 import org.lwjgl.opengl.GL11.*
-import org.lwjgl.opengl.GL12.*
-import org.lwjgl.opengl.GL13.*
-import org.lwjgl.opengl.GL14.*
-import org.lwjgl.opengl.GL15.*
-import org.lwjgl.opengl.GL20.*
-import org.lwjgl.opengl.GL21.*
 import org.lwjgl.stb.STBImage.*
 import org.lwjgl.system.MemoryStack
 import java.nio.IntBuffer
 import java.nio.ByteBuffer
 
-class Texture(var filePath: String){
+class Texture(private val filePath: String){
     private var textureId: Int = -1
 
     fun load(): Boolean{
-        val stack = MemoryStack.create()
-        val w: IntBuffer = stack.mallocInt(1)
-        val h: IntBuffer = stack.mallocInt(1)
-        val comp: IntBuffer = stack.mallocInt(1)
-        stbi_set_flip_vertically_on_load(true)
-        val image: ByteBuffer = stbi_load(filePath, w, h, comp, 4) ?: return false
-        val width = w.get()
-        val height = h.get()
+        MemoryStack.stackPush().use{
+            val w: IntBuffer = it.mallocInt(1)
+            val h: IntBuffer = it.mallocInt(1)
+            val comp: IntBuffer = it.mallocInt(1)
+            stbi_set_flip_vertically_on_load(true)
+            val image: ByteBuffer = stbi_load(filePath, w, h, comp, 4) ?: return false
+            val width = w.get()
+            val height = h.get()
 
-        textureId = glGenTextures()
-        glBindTexture(GL_TEXTURE_2D, textureId)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+            textureId = glGenTextures()
+            glBindTexture(GL_TEXTURE_2D, textureId)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
 
-        return true
+            return true
+        }
     }
 }
